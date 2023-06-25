83632a4e9d7fbe850088eef6fcc15f9d81b85f63
StepsOnes <stepan663663@gmail.com>
2023-06-26 01:54:34 +0300

    update cdn

--- src/core/utils/constance/index.ts
+++ src/core/utils/constance/index.ts
@@ -5,4 +5,4 @@ export const MINT_GEN_ZERO_ADDRESS =
 export const MINT_GEN_FIRST_ADDRESS =
 	'0x38951d304E3579005dB3D8D932077f2A6E48A500';
 export const IMAGE_DELIVERY_URL = 'https://imagedelivery.net/HmgqhtdzVbRs-_BPsHnYHg/';
-export const CDN_URL = 'https://cryptoflats.static.fra1.cdn.digitaloceanspaces.com';
+export const CDN_URL = 'https://cryptoflatss.fra1.cdn.digitaloceanspaces.com';
