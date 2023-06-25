0cdd9b89bda3b9b917d1c7bf27e784af98854e44
StepsOnes <stepan663663@gmail.com>
2023-06-26 02:42:47 +0300

    yandex metrica add

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -51,6 +51,26 @@ export default async function LocaleLayout({ children, params: { locale } }: Pro
 			/>
 			<meta property="og:image" content="https://imagedelivery.net/HmgqhtdzVbRs-_BPsHnYHg/ea0d8174-f4c5-4de5-d8d0-233c40381500/cover" />
 			<meta name="yandex-verification" content="90ddaeffb810e21a" />
+			<Script id='yandex-metrika'>
+				{`
+					<!-- Yandex.Metrika counter -->
+					<script type="text/javascript" >
+						 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+						 m[i].l=1*new Date();
+						 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+						 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+						 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+					
+						 ym(94107530, "init", {
+									clickmap:true,
+									trackLinks:true,
+									accurateTrackBounce:true
+						 });
+					</script>
+					<noscript><div><img src="https://mc.yandex.ru/watch/94107530" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+					<!-- /Yandex.Metrika counter -->
+				`}
+			</Script>
 		</head>
 		<body>
 		<Script src='https://www.googletagmanager.com/gtag/js?id=G-DW47EZWVL0' />
