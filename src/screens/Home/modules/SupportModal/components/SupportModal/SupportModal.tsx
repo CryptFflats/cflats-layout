6d3e7016f222c6b5bf5f4923a7cd26185d41df61
StepsOnes <stepan663663@gmail.com>
2023-07-29 02:32:45 +0300

    bag-fix

--- src/screens/Home/modules/SupportModal/components/SupportModal/SupportModal.tsx
+++ src/screens/Home/modules/SupportModal/components/SupportModal/SupportModal.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import { Dialog } from '@mui/material';
 import { SupportDialog, SupportDialogContent } from './styles';
-import ReCAPTCHA from "react-google-recaptcha";
 import * as process from 'process';
 import { useForm } from 'react-hook-form';
 import Header from '../Header/Header';
@@ -12,7 +11,6 @@ import SupportForm from '../SupportForm/SupportForm';
 const SupportModal = () => {
 	const { handleSubmit, register } = useForm()
 	const [open, setOpen] = useState<boolean>(true);
-	const SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string
 
 	const handleChange = (value: any) => {
 		console.log(value)
