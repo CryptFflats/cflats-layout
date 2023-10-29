70a704247e2cf0d6738e14e89f9826861f9fffde
Dan Pisarev <pisarev@softlab.ru>
2023-10-29 21:35:58 +0300

    refactor: применяет prettier к файлу

--- src/core/store/slices/ModalSlice.ts
+++ src/core/store/slices/ModalSlice.ts
@@ -1,32 +1,33 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface MenuState {
-	developmentModal: boolean
-	trailerModal: boolean
-	supportModal: boolean
+	developmentModal: boolean;
+	trailerModal: boolean;
+	supportModal: boolean;
 }
 
 const initialState: MenuState = {
 	developmentModal: false,
 	trailerModal: false,
 	supportModal: false
-}
+};
 
 const ModalSlice = createSlice({
 	name: 'ModalSlice',
 	initialState,
 	reducers: {
 		setDevelopment(state, action: PayloadAction<boolean>) {
-			state.developmentModal = action.payload
+			state.developmentModal = action.payload;
 		},
 		setTrailer(state, action: PayloadAction<boolean>) {
-			state.trailerModal = action.payload
+			state.trailerModal = action.payload;
 		},
 		setSupportModal(state, action: PayloadAction<boolean>) {
-			state.supportModal = action.payload
+			state.supportModal = action.payload;
 		}
 	}
-})
+});
 
-export const { setDevelopment, setTrailer, setSupportModal } = ModalSlice.actions
-export default ModalSlice.reducer
+export const { setDevelopment, setTrailer, setSupportModal } =
+	ModalSlice.actions;
+export default ModalSlice.reducer;
